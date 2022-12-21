dfc43773fa687c00f4994a17d23eae6e1ddf17e0
Keunoh <rmsdhcjswo1@gmail.com>
2022-12-21 23:48:39 +0900

    두 수의 나눗셈

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,18 @@ import java.util.Arrays;
 public class Main {
 
     //조건1. 정수 num1과 num2가 매개변수로 주어짐
-    //조건2. 두 정수의 합을 return
+    //조건2. num1을 num2로 나눈 값에 1000을 곱하고 정수 부분 return
 
-    public int solution(int num1, int num2) {
-        return num1 + num2;
+
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 2;
+
+        double value = (a * 1.0) / b;
+        double result = value * 1000;
+
+        int result1 = (int) result;
+        System.out.println("result1 = " + result1);
+
     }
 }
---
+++ src/level0/Divide.java
@@ -0,0 +1,9 @@
+package level0;
+
+public class Divide {
+
+    public int solution(int num1, int num2) {
+        double value = ((num1 * 1.0) / num2) * 1000;
+        return (int) value;
+    }
+}
