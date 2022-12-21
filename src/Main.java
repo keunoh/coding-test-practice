5b567e5b1ffd4f3d671845d545b00442a8ba9c02
Keunoh <rmsdhcjswo1@gmail.com>
2022-12-21 23:51:03 +0900

    숫자 비교하기

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,15 @@ import java.util.Arrays;
 
 public class Main {
 
-    //조건1. 정수 num1과 num2가 매개변수로 주어짐
-    //조건2. num1을 num2로 나눈 값에 1000을 곱하고 정수 부분 return
-
-
     public static void main(String[] args) {
-        int a = 3;
-        int b = 2;
+        int a = 2;
+        int b = 3;
 
-        double value = (a * 1.0) / b;
-        double result = value * 1000;
-
-        int result1 = (int) result;
-        System.out.println("result1 = " + result1);
-
+        if (a == b) {
+            System.out.println(1);;
+        } else {
+            System.out.println(-1);
+        }
     }
+
 }
---
+++ src/level0/CompareNumber.java
@@ -0,0 +1,12 @@
+package level0;
+
+public class CompareNumber {
+
+    public int solution(int num1, int num2) {
+        if (num1 == num2) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+}
