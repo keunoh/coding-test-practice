aad6c2e22267306eebc41e9523f2124aa651953f
Keunoh <rmsdhcjswo1@gmail.com>
2022-12-23 01:25:31 +0900

    최빈값 구하기

--- src/level0/MostAppearValue.java
+++ src/level0/MostAppearValue.java
@@ -1,30 +1,12 @@
+package level0;
+
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 
-public class Main {
+public class MostAppearValue {
 
-    //최빈값은 주어진 값 중에서 가장 자주 나오는 값을 의미함.
-    // 정수 배열 array 주어짐
-    // 최빈값을 return, 최빈값이 여러개면 -1리턴
-
-    /**
-     * 1. 배열의 첫 번째 요소를 두 번째 요소부터 마지막까지 확인하고 같으면 카운트 높여 줌 (ok)
-     * 2. 첫 번째 요소부터 현재 요소까지 비교해서 현재 요소 값과 같은 값이 있다면 검색하지 않음 (ok)
-     * 3. 해당 인덱스의 값과 카운트를 맵으로 담아 둠 (ok)
-     * 4. 밸류의 최대값을 가져와서 해당 밸류의 키값을 리턴함
-     * 5. 만약 최대값이 두 개 이상 존재하면 -1 반환
-     * @param args
-     */
-
-    public static void main(String[] args) {
-        int[] arr = {1,1,1};
-
-        System.out.println("answer = " + solution(arr));
-    }
-
-    public static int solution(int[] array) {
+    public int solution(int[] array) {
         return mapping(array);
     }
 
@@ -33,10 +15,11 @@ public class Main {
 
         for (int i = 0; i < arr.length; i++) {
             int count = 1;
+            int k = 0;
             //조건2
             if ((i != 0)){
                 int isBeforeSame = 0;
-                for (int k = 0; k < i; k++) {
+                for (k = 0; k < i; k++) {
                     if (arr[i] == arr[k]) {
                         isBeforeSame++;
                     }
